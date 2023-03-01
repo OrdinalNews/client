669b2ca49542f0cecbc59aeb147553dfa6d23503
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-03-01 07:09:25 -0700

    fix: convert landing page to use buttons

--- src/routes/root-index.tsx
+++ src/routes/root-index.tsx
@@ -1,9 +1,13 @@
-import { Box, Heading, Text, Link as ChakraLink, Image } from '@chakra-ui/react';
+import { Box, Heading, Text, Link as ChakraLink, Image, Button } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
 export default function RootIndex() {
   return (
-    <Box borderRadius="xl">
+    <Box
+      borderRadius="xl"
+      display="flex"
+      flexDir="column"
+    >
       <Image
         boxSize="150px"
         margin="0 auto"
@@ -11,18 +15,39 @@ export default function RootIndex() {
         alt="Ordinal News Logo"
       />
       <Heading>Ordinal News Standard</Heading>
-
-      <Text fontSize="xl">Setting the standard for inscribing news on Bitcoin.</Text>
-      <br />
-      <Text>
-        <Link to="/post-news">Inscribe the News</Link> |{' '}
-        <ChakraLink
+      <Text
+        fontSize="xl"
+        mb={8}
+      >
+        Setting the standard for inscribing news on Bitcoin.
+      </Text>
+      <Box
+        display="flex"
+        flexDir={['column', 'column', 'row']}
+        alignContent="center"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Button
+          as={Link}
+          to="/post-news"
+          className="link-button"
+          me={[0, 0, 8]}
+          mb={[8, 8, 0]}
+          w={['fit-content', '75%', 'auto']}
+        >
+          Inscribe the News
+        </Button>
+        <Button
+          as={ChakraLink}
           href="https://docs.inscribe.news"
+          className="link-button"
           isExternal
+          w={['fit-content', '75%', 'auto']}
         >
           Read the Docs
-        </ChakraLink>
-      </Text>
+        </Button>
+      </Box>
     </Box>
   );
 }
