dce5920f1aeb494fcd34a5979be739249dd5dadf
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-03-07 15:25:26 -0700

    fix: save that one last open file

--- functions/api.ts
+++ functions/api.ts
@@ -1,4 +1,4 @@
-import { createResponse } from '../../lib/api-helpers';
+import { createResponse } from '../lib/api-helpers';
 
 export async function onRequest(): Promise<Response> {
   return createResponse(`Welcome to the Ordinal News Indexer API instance!`);
