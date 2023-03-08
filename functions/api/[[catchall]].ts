85c027d256812b20fa829e87e79422e5f64acb51
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-03-08 12:19:44 -0700

    fix: remove api index route and update catchall

--- functions/api/[[catchall]].ts
+++ functions/api/[[catchall]].ts
@@ -2,7 +2,9 @@ import { createResponse } from '../../lib/api-helpers';
 
 export async function onRequest(): Promise<Response> {
   return createResponse(
-    `Welcome to the Ordinal News Indexer! Page not found. Docs coming soon.`,
-    404
+    `Welcome to the Ordinal News Indexer! Supported routes below:\n\n
+    /api/inscription/INSCRIPTION_ID - returns inscription data\n
+    /api/content/INSCRIPTION_ID - returns inscription content\n
+    /api/news/INSCRIPTION_ID - returns html from news inscription`
   );
 }
