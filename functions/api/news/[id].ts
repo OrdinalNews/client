a2a6626d0328082a90a892a2d7ce88803adfc15b
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-03-07 17:06:24 -0700

    fix: add markdown-it and test formatting

--- functions/api/news/[id].ts
+++ functions/api/news/[id].ts
@@ -1,10 +1,12 @@
 import { createResponse, fetchUrl, OrdinalNews, ordinalsUrlBase } from '../../../lib/api-helpers';
+import MarkdownIt from 'markdown-it';
 
 export async function onRequest(context: any): Promise<Response> {
   const url = new URL(`/content/${context.params.id}`, ordinalsUrlBase);
   const content = await fetchUrl(url.toString()).catch(() => undefined);
   if (content === undefined || Object.keys(content).length === 0)
     return createResponse(`Content for ID ${context.params.id} not found.`, 404);
+  const md = new MarkdownIt();
   const news: OrdinalNews = {
     p: content.p,
     op: content.op,
@@ -13,8 +15,13 @@ export async function onRequest(context: any): Promise<Response> {
     body: content.body,
     author: content.author,
   };
-  return new Response(JSON.stringify(news, null, 2), {
+  let formattedNews = `<h1>${news.title}</h1>`;
+  if (news.url) formattedNews += `\nURL: <a href="${news.url}">${news.url}</a>`;
+  if (news.author)
+    formattedNews += `\n<span style="font-style:italic">Author: ${news.author}</span>`;
+  if (news.body) formattedNews += `<hr />\n${md.render(news.body)}`;
+  return new Response(formattedNews, {
     status: 200,
-    headers: { 'Content-Type': 'application/json' },
+    headers: { 'Content-Type': 'text/html' },
   });
 }
