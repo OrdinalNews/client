9cfd308d7a7bd366cdb6891fc55325d3972f15e1
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-03-08 17:18:01 -0700

    fix: rename /inscription route to /info

--- functions/api/[[catchall]].ts
+++ functions/api/[[catchall]].ts
@@ -5,9 +5,10 @@ export async function onRequest({ request }): Promise<Response> {
   const path = url.pathname;
   return createResponse(
     `Welcome to the Ordinal News Indexer and API! Supported routes below:\n\n
-  /api/inscription/INSCRIPTION_ID - returns inscription data\n
+  /api/info/INSCRIPTION_ID - returns inscription data\n
   /api/content/INSCRIPTION_ID - returns inscription content\n
-  /api/news/INSCRIPTION_ID - returns html from news inscription\n\n
+  /api/news/INSCRIPTION_ID - returns html from news inscription\n
+  /api/data - returns all known keys in KV\n\n
 current path: ${path}`
   );
 }
