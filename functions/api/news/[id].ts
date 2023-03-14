2d3968efb2a7992de91173beb3230bf238f4483e
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-03-14 08:35:16 -0700

    fix: update news endpoint to return body as html
    Instead of returning a full page, only return the body as HTML, which could be queried/used in a viewer component in the normal site directory outside of the API.

--- functions/api/news/[id].ts
+++ functions/api/news/[id].ts
@@ -1,24 +1,20 @@
-import { createResponse, fetchUrl, ordinalsUrlBase } from '../../../lib/api-helpers';
-import { Env, OrdinalNews } from '../../../lib/api-types';
 import MarkdownIt from 'markdown-it';
 import { EventContext } from '@cloudflare/workers-types';
-import { getOrFetchInscriptionContent } from '../content/[id]';
+import { createResponse, getInscription } from '../../../lib/api-helpers';
+import { Env, OrdinalNews } from '../../../lib/api-types';
 
 export async function onRequest(context: EventContext<Env, any, any>): Promise<Response> {
   const { env } = context;
   const id = String(context.params.id);
-  const contentData = await getOrFetchInscriptionContent(env, id).catch(err => {
-    console.log(`err: ${err}`);
-    return undefined;
-  });
+  const inscriptionData = await getInscription(env, id);
   if (
-    contentData === undefined ||
-    Object.keys(contentData).length === 0 ||
-    contentData.content.body === null
+    inscriptionData === undefined ||
+    Object.keys(inscriptionData).length === 0 ||
+    inscriptionData.content.body === null
   ) {
     return createResponse(`Inscription content not found for ${id}`, 404);
   }
-  const content = await contentData.content.text();
+  const content = await inscriptionData.content.text();
   let news: OrdinalNews;
   try {
     const contentObj = JSON.parse(content);
@@ -29,13 +25,27 @@ export async function onRequest(context: EventContext<Env, any, any>): Promise<R
       url: contentObj.url,
       body: contentObj.body,
       author: contentObj.author,
+      authorAddress: contentObj.authorAddress,
+      signature: contentObj.signature,
     };
   } catch (err) {
     return createResponse(`Unable to parse news inscription for ${id}`, 404);
   }
 
   const md = new MarkdownIt();
-  const htmlStart = `<html><head><title>${news.title} - Ordinal News Standard</title><style>body { background-color: #000; color: #fff; font-weight: 350; line-height: 1.6; font-size: 1.5em; } a, a:active, a:visited { color: #1eaab4 } a:hover { color: #2ad0db }</style></head><body>`;
+  if (!news.body) {
+    return createResponse(`No body found for ${id}`, 404);
+  }
+  return new Response(md.render(news.body), {
+    status: 200,
+    headers: { 'Content-Type': 'text/html' },
+  });
+}
+
+/*
+OLD CODE RETURNING HTML PAGE
+
+const htmlStart = `<html><head><title>${news.title} - Ordinal News Standard</title><style>body { background-color: #000; color: #fff; font-weight: 350; line-height: 1.6; font-size: 1.5em; } a, a:active, a:visited { color: #1eaab4 } a:hover { color: #2ad0db }</style></head><body>`;
   let formattedNews = `<h1>${news.title}</h1>`;
   if (news.author) formattedNews += `<span style="font-style:italic">Author: ${news.author}</span>`;
   if (news.url) formattedNews += `<br /><a href="${news.url}">${news.url}</a>`;
@@ -46,4 +56,5 @@ export async function onRequest(context: EventContext<Env, any, any>): Promise<R
     status: 200,
     headers: { 'Content-Type': 'text/html' },
   });
-}
+
+*/
