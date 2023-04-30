cf1275d61175fc202fd7e4663b1baef5954cc115
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-04-30 11:16:27 -0700

    docs: update root api landing page

--- functions/api/[[catchall]].ts
+++ functions/api/[[catchall]].ts
@@ -4,11 +4,13 @@ export async function onRequest({ request }): Promise<Response> {
   const url = new URL(request.url);
   const path = url.pathname;
   return createResponse(
-    `Welcome to the Ordinal News Indexer and API! Supported routes below:\n\n
-  /api/info/INSCRIPTION_ID - returns inscription data (all)\n
-  /api/content/INSCRIPTION_ID - returns inscription content (all)\n
-  /api/news/INSCRIPTION_ID - returns html from news inscription body (news only)\n
-  /api/data/INSCRIPTION_ID - returns inscription data and content (news only)\n
+    `Welcome to the Ordinal News Indexer and API!\n\n
+    More information can be found in the docs: https://docs.inscribe.news/api\n\n
+    Supported routes below, where ID = inscription ID or number:\n\n
+  /api/info/ID - returns inscription data (all)\n
+  /api/content/ID - returns inscription content (all)\n
+  /api/news/ID - returns html from news inscription body (news only)\n
+  /api/data/ID - returns inscription data and content (news only)\n
   /api/data/ord-news - returns all known news inscriptions in KV\n
   /api/data/ord-list - returns all known inscriptions in KV\n\n
 current path: ${path}`
